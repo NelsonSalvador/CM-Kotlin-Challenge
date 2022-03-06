4efdd6122aa19e4bea26e4c17ef766f2a2210356
Nelson Salvador <nelsonsalvadorzx@gmail.com>
2022-03-06 18:02:22 +0000

    Pessoa.kt Code

--- src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
@@ -1,4 +1,47 @@
 package pt.ulusofona.cm.kotlin.challenge.pt.ulusofona.cm.kotlin.challenge.models
+import java.util.*
+import kotlin.collections.*
 
-class Pessoa {
+data class Pessoa (val nome: String,val dataDeNascimento: Date){
+    var veiculos = mutableListOf<Veiculo>()
+    var carta: Carta? = null
+    var posicao: Posicao? = null
+
+    fun comprarVeiculo(veiculo: Veiculo){
+        veiculos.add(veiculo)
+    }
+
+    fun pesquisarVeiculo(identificador: String): Veiculo{
+        //Finish fun
+        for(veiculo in veiculos){
+            if(veiculo.identificador == identificador){
+                return veiculo
+            }
+        }
+        // This is wrong, Needs exeption
+        return veiculos[0]
+    }
+
+    fun venderVeiculo(identificador: String, comprador: Pessoa){
+        for(veiculo in veiculos){
+            if(veiculo.identificador == identificador){
+                //Removes veicle from the pessoa
+                veiculos.remove(veiculo)
+                //Add veicle to the buyer
+                comprador.veiculos.add(veiculo)
+            }
+        }
+    }
+
+    fun moverVeiculoPara(identificador: String, x: Int, y: Int){
+
+    }
+
+    fun temCarta(): Boolean{
+        return carta != null
+    }
+
+    fun tirarCarta(){
+        carta = Carta()
+    }
 }
--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -1,4 +1,11 @@
 package pt.ulusofona.cm.kotlin.challenge.pt.ulusofona.cm.kotlin.challenge.models
+import pt.ulusofona.cm.kotlin.challenge.pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
+import java.util.*
+import kotlin.collections.*
 
-class Veiculo {
+open class Veiculo(val identificador: String): Movimentavel {
+    //val identificador get() = _identificador
+    var posicao: Posicao = Posicao()
+
+
 }
